6f05aeaab765cc98453ae5e593e23f196043e0c4
Cristian <duquec@sunypoly.edu>
2025-02-02 20:45:06 -0500

    Creation of Additional Card Scripts
    CardUI and ScriptableCard scripts for future development

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,8 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* Purpose:
+ * Define what a card is and can be, while connecting all data and behaviours.
+ * 
+ * Writer: Cristian Duque
+ * 
+ * Scripts borrowing from it:
+ * Deck Builder.cs
+ * 
+ * Scripts it borrows from:
+ * 
+ */
+
 public class Card : MonoBehaviour
 {
+    // Fields and Properties
+    [field: SerializeField] public ScriptableCard CardData { get; private set; }
+
+    // Methods
+
     // Start is called before the first frame update
     void Start()
     {
