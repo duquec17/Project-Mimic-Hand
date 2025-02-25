9f898d34e920d63061261baa028e0ac2955166a3
Cristian <duquec@sunypoly.edu>
2025-02-25 18:47:48 -0500

    Creation of Player Controls Script
    Starting to create script that will handle majority or all player controls when in-level.

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Purpose:
+ * Handles basic player Controls. Examples 8 direction movement (Up & down for passing through planes).
+ * The key binds that determine which card is used from the hand. 
+ * 
+ * Writer: Cristian Duque
+ * ------------------------------------
+ * Scripts borrowing from it:
+ * 
+ * ------------------------------------
+ * Scripts it borrows from:
+ * 
+ */
+public class PlayerControls : MonoBehaviour
+{
+    // Variable List
+    public float MoveSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
