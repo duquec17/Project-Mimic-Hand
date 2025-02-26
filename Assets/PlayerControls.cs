1f97c08f9ca46da6c354066648cf089e16ef3832
Cristian <duquec@sunypoly.edu>
2025-02-25 19:10:41 -0500

    Adding pieces

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -18,17 +18,29 @@ public class PlayerControls : MonoBehaviour
 {
     // Variable List
     public float MoveSpeed;
+    float speedX, speedY;
+    Rigidbody2D rb;
+
+    /* Algorithm
+     * 1. Assign speed to player
+     * 2. Assign keys to directions
+     *  a. AWSD for direction buttons
+     *  b. UIOPJ for attack buttons
+     * 3. 
+     */
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        speedX = Input.GetAxisRaw("Horizontal") * MoveSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * MoveSpeed;
+        rb.velocity = new Vector2(speedX, speedY);
     }
 }
