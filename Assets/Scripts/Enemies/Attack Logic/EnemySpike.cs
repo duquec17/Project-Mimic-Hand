ac6d6e43273a006b17fa75cc7cf99fa65ee12b4a
Cristian <duquec@sunypoly.edu>
2025-04-07 15:50:36 -0400

    Update EnemySpike.cs

--- Assets/Scripts/Enemies/Attack Logic/EnemySpike.cs
+++ Assets/Scripts/Enemies/Attack Logic/EnemySpike.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class EnemySpike : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Spike variable list
+    [SerializeField] private float warningDuration = 1f; // Time before spike activates
+    [SerializeField] private float activeDuration = 1f; // Time spike is active
+    [SerializeField] private float damage = 15f;
+    [SerializeField] private SpriteRenderer spriteRenderer; // For color change might remove
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private bool isActive = false;
 }
