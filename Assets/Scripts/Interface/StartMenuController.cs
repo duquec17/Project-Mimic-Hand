bc69ec6f46beff297e75d6bd84e7f0a97fb88950
Cristian <duquec@sunypoly.edu>
2025-05-21 19:26:22 -0400

    Creation of Text Tutorial Menu
    - Operational
    - Updating text box tied to button press
    -

--- Assets/Scripts/Interface/StartMenuController.cs
+++ Assets/Scripts/Interface/StartMenuController.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class StartMenuController : MonoBehaviour
 {
+    // Start Menu specific
     public void OnStartClick()
     {
         SceneManager.LoadScene("DeckBuilder");
@@ -26,5 +29,27 @@ public class StartMenuController : MonoBehaviour
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
         Application.Quit();
+    }
+
+    // Tutorial Menu Specific
+    public TextMeshProUGUI phase_Text;
+    public void OnNextClick()
+    {
+        phase_Text.text = "Phase 2: In-Level " +
+            "\n- On entering the level, player moves using the AWSD key to go left, up, down, and right.";
+    }
+
+    public void OnPreviousClick()
+    {
+        phase_Text.text = "Phase 1: Deck Builder" +
+            "\n- A total of 20 cards will appear, of them use the mouse to click and select 20." +
+            "\n- Each card has a unique effect (in-development)." +
+            "\n- Once all 15 are selected the level can be started." +
+            "\n- Each card is used alongside an attack.";
+    }
+
+    public void OnMenuClick()
+    {
+        SceneManager.LoadScene("Start Menu");
     }
 }
